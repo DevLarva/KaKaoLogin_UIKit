b36b24d1e24541afe6e7edd4e18f533e5bae470b
백대홍 <bdh3620@naver.com>
2022-11-02 21:30:51 +0900

    setting

---
+++ KaKaoLogin(UIKit)-tuto/KakaoAuthVM.swift
@@ -0,0 +1,12 @@
+//
+//  KakaoAuthVM.swift
+//  KaKaoLogin(UIKit)-tuto
+//
+//  Created by 백대홍 on 2022/11/02.
+//
+
+import Foundation
+import Combine
+import KakaoSDKAuth
+import KakaoSDKUser
+
---
+++ KaKaoLogin(UIKit)-tuto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KaKaoLogin(UIKit)-tuto
+//
+//  Created by 백대홍 on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
