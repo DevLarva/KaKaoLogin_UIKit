89afa8e24db947ed9c117e14b8b052ac8968d585
백대홍 <bdh3620@naver.com>
2022-11-03 15:57:41 +0900

    second Commit

--- KaKaoLogin(UIKit)-tuto/KakaoAuthVM.swift
+++ KaKaoLogin(UIKit)-tuto/KakaoAuthVM.swift
@@ -10,3 +10,44 @@ import Combine
 import KakaoSDKAuth
 import KakaoSDKUser
 
+class KaKaoAuthVM: ObservableObject {
+    var subscription = Set<AnyCancellable>()
+    
+    init() {
+        print("kakaoAuthVM init() called")
+    }
+    
+    func handleKaKaoLogin() {
+        print("KakaoAuthVM - handleKakaoLogin() called")
+        // 카카오톡 설치 여부 확인
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+
+                    //do something
+                    _ = oauthToken
+                }
+            }
+        }
+            // 카톡이 설치되지 않은 경우
+        else {
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                    if let error = error {
+                        print(error)
+                    }
+                    else {
+                        print("loginWithKakaoAccount() success.")
+
+                        //do something
+                        _ = oauthToken
+                    }
+                }
+        }
+
+    }
+}
+
--- KaKaoLogin(UIKit)-tuto/ViewController.swift
+++ KaKaoLogin(UIKit)-tuto/ViewController.swift
@@ -6,14 +6,106 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var kakaoLoginStatusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "로그인 여부 라벨"
+        return label
+        
+    }()
+    
+    lazy var kakaoLoginButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("카카오 로그인", for: .normal)
+        btn.configuration = .filled()
+        btn.addTarget(self, action: #selector(loginBtnClicked), for: .touchUpInside)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+        
+    }()
+    
+    lazy var kakaoLogoutButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("카카오 로그아웃", for: .normal)
+        btn.configuration = .filled()
+        btn.addTarget(self, action: #selector(logoutBtnClicked), for: .touchUpInside)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+        
+    }()
+    
+    lazy var stackView : UIStackView = {
+        let stack = UIStackView()
+        stack.spacing = 8
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.distribution = .fill
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stack
+    }()
+    
+    lazy var kakaoAuthVM: KaKaoAuthVM = { KaKaoAuthVM() }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        kakaoLoginStatusLabel.snp.makeConstraints { make in
+            make.height.greaterThanOrEqualTo(80)
+        }
+        stackView.addArrangedSubview(kakaoLoginStatusLabel)
+        stackView.addArrangedSubview(kakaoLoginButton)
+        stackView.addArrangedSubview(kakaoLogoutButton)
+        
+        self.view.addSubview(stackView)
+        
+        stackView.snp.makeConstraints { make in
+            
+            make.center.equalTo(self.view)
+        }
+    }
+    
+    
+    //MARK: -버튼액션
+    @objc func loginBtnClicked() {
+        print("LoginBtnClicked() called")
+        kakaoAuthVM.handleKaKaoLogin()
+    }
+    @objc func logoutBtnClicked() {
+        print("logoutBtnClicked() called")
     }
 
+} //ViewController
 
+#if DEBUG
+
+
+import SwiftUI
+
+
+struct ViewControllerPresentable: UIViewControllerRepresentable {
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    func makeUIViewController(context: Context) -> some UIViewController {
+        ViewController()
+        
+    }
 }
 
+
+
+
+struct ViewControllerPresentable_PreviewProvider : PreviewProvider {
+    static var previews: some View {
+        ViewControllerPresentable()
+        
+    }
+}
+
+#endif
