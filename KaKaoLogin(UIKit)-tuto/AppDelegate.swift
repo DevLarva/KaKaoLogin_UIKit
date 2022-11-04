020c96ef16e9b3546177437420936b3018b2e4d7
백대홍 <bdh3620@naver.com>
2022-11-05 00:50:59 +0900

    bug

--- KaKaoLogin(UIKit)-tuto/AppDelegate.swift
+++ KaKaoLogin(UIKit)-tuto/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-           if (AuthApi.isKakaoTalkLoginUrl(url)) {
+           if (AuthApi.isKakaoTalkLoginUrl(url)) { 
                return AuthController.handleOpenUrl(url: url)
            }
 
